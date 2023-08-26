e98200807322f7a15ab79ae4e7d08454b6f27ae0
kaxhay <201370031@gift.edu.pk>
2023-08-26 12:35:25 +0500

    updated

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Akasha Farrukh 201370031</Text>
+      <Text>Open js to work</Text>
       <StatusBar style="auto" />
     </View>
   );
